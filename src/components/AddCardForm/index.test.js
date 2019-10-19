2715193c032b0c1753a901eafcfbd4812692a993
Jo Sprague <josiah.sprague@gmail.com>
2019-10-19 20:08:58 +0200

    Add enzyme tests for AddCardForm
    Installed enzyme and added tests that check that AddCardForm has
    all expected inputs.
    I added these while working on the form, but committed later.

--- src/components/AddCardForm/index.test.js
+++ src/components/AddCardForm/index.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import AddCardForm from '.';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AddCardForm />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import AddCardForm from './index';
+
+describe('AddCard Form', function() {
+  it('renders a form tag', function() {
+    expect(shallow(<AddCardForm />).exists('form')).toBe(true);
+  });
+
+  it('renders all inputs', function() {
+    expect(shallow(<AddCardForm />).find('input#title').length).toBe(1);
+    expect(shallow(<AddCardForm />).find('input#email').length).toBe(1);
+    expect(shallow(<AddCardForm />).find('#description').length).toBe(1);
+  });
+
+  it('renders two buttons', function() {
+    expect(shallow(<AddCardForm />).find('button').length).toBe(2);
+  });
 });
