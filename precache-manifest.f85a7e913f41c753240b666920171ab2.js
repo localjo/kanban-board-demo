0eb06d91994775b62a6650340bd70824ca80621a
Jo Sprague <josiah.sprague@gmail.com>
2019-11-10 14:28:33 +0100

    Updates

---
+++ precache-manifest.f85a7e913f41c753240b666920171ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2571d5d04c4d6541826fd358b9a7ec39",
+    "url": "/kanban-takehome/index.html"
+  },
+  {
+    "revision": "b61b83d4a3c55dae4713",
+    "url": "/kanban-takehome/static/css/main.d0552a5c.chunk.css"
+  },
+  {
+    "revision": "4880151c556de9bf9025",
+    "url": "/kanban-takehome/static/js/2.750f2fff.chunk.js"
+  },
+  {
+    "revision": "b61b83d4a3c55dae4713",
+    "url": "/kanban-takehome/static/js/main.a72ef245.chunk.js"
+  },
+  {
+    "revision": "b8ede479a1803ca5de3f",
+    "url": "/kanban-takehome/static/js/runtime~main.9aadc6ec.js"
+  }
+]);
